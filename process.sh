45801c7b9cc011f3d51087b2673fed3dabfa1c19
Harim Park <fow@netspheres.org>
2020-07-24 15:41:37 +0900

    Update process.sh
    add the process the Gradient warter mark removal.

--- process.sh
+++ process.sh
@@ -12,20 +12,31 @@ if [ -d "$DIR" ]; then
 	cd "$DIR"
 	mkdir raw
 	mkdir clean
-	
+	mkdir clean-tmp
+
 	mv *.png ./raw
 	cd raw
-	
-	echo "Removing watermarks"
+
+	echo "Removing watermarks #1"
 	for f in *.png
 	do
-		convert $f -fuzz 8% -fill white -opaque "#e6e6e6" ../clean/$f
+		convert $f -fuzz 8% -fill white -opaque "#e6e6e6" ../clean-tmp/$f
 	done
-	
+
 	cd ..
+	cd clean-tmp
+
+	echo "Removing watermarks #2"
+	for f in *.png
+	do
+		convert $f -fuzz 8% -fill white -opaque "#bdbdbd" ../clean/$f
+	done
+
+	cd ..
+	rm -rf clean-tmp
 	cp -R clean numbered
 	cd numbered
-	
+
 	echo "Numbering pages"
 	for f in *.png
 	do
